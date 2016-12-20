ddef7965afdbecae229b940500a902c7dd502f97
Kevin Sogata <ksogata@gmail.com>
2016-12-19 19:48:32 -0800

    removed some files from repo.

--- src/page_elements.py
+++
@@ -1,9 +0,0 @@
-from src.element_classes import *
-
-class PageElements(object):
-    def __init__(self, driver):
-        # Driver for testing JavaScript elements (can be Browser or Headless)
-        self.driver = driver
-        # Button to close the About popup box on every page on first visit
-        self.popup_close = ElementByClassName('ui-dialog-titlebar-close',
-            self.driver)
